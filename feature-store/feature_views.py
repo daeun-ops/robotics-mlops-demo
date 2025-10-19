c49f6ae4f3d1b5f8f7abf0bdaecd285ebe356852
Daeun(Sophie) Kim <euncd61@gmail.com>
2025-10-19 16:17:53 +0900

    Update feature_views.py

--- feature-store/feature_views.py
+++ feature-store/feature_views.py
@@ -1,10 +1,10 @@
 from feast import Field, FeatureView, FileSource
-from feast.types import Float32, Int64
+from feast.types import Float32
 source = FileSource(path="./data/features.parquet", timestamp_field="timestamp")
 robot_features = FeatureView(
-    name="robot_features",
-    entities=["robot_id"],
-    ttl=None,
-    schema=[Field(name="acc_mean", dtype=Float32()), Field(name="gyro_mean", dtype=Float32())],
-    source=source
+  name="robot_features",
+  entities=["robot_id"],
+  ttl=None,
+  schema=[Field(name="acc_mean", dtype=Float32()), Field(name="gyro_mean", dtype=Float32())],
+  source=source
 )
